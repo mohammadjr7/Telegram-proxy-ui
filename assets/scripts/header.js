979ac222670156693485a5aababd63fe1aba90ee
Mohammad Javad <mrakhshani88@gmail.com>
2023-06-03 20:10:34 +0330

    UI Improvement: better head UX, auto hide when scrolling

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,12 +1,22 @@
 let position = window.scrollY;
+let previousPosition = 0;
 let header = document.getElementById("header")
 
 window.addEventListener("scroll", (event) => {
     position = this.scrollY;
     if (position != 0) {
+        header.classList.add("hidden")
         header.classList.add("small")
     } else {
         header.classList.remove("small")
+        header.classList.remove("hidden")
     }
+
+    if (previousPosition < position) {
+        header.classList.add("hidden")
+    } else {
+        header.classList.remove("hidden")
+    }
+    previousPosition = position;
 });
 
