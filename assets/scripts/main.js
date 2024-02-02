3830154b559612aa970b6eaf2af2635d74ef4400
Mohammad Javad <mrakhshani88@gmail.com>
2024-02-02 14:28:21 +0330

    [FEATURE]: Add an animation for Change Theme button animataion

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,20 @@
 const CONTAINER = document.getElementById("container");
 const URL = "https://raw.githubusercontent.com/hookzof/socks5_list/master/tg/mtproto.json"
+
+const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" /></svg>`;
+const sunIcon = `<!-- https://feathericons.dev/?search=sun&iconset=feather -->
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="main-grid-item-icon" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+  <circle cx="12" cy="12" r="5" />
+  <line x1="12" x2="12" y1="1" y2="3" />
+  <line x1="12" x2="12" y1="21" y2="23" />
+  <line x1="4.22" x2="5.64" y1="4.22" y2="5.64" />
+  <line x1="18.36" x2="19.78" y1="18.36" y2="19.78" />
+  <line x1="1" x2="3" y1="12" y2="12" />
+  <line x1="21" x2="23" y1="12" y2="12" />
+  <line x1="4.22" x2="5.64" y1="19.78" y2="18.36" />
+  <line x1="18.36" x2="19.78" y1="5.64" y2="4.22" />
+</svg>`;
+
 var proxies = "";
 var html = "";
 var theme = "light";
@@ -81,6 +96,7 @@ function copyToClipboard(text, id) {
 
 function changeTheme() {
     if (theme == "light") {
+
         body = document.getElementById("body");
         body.classList.add("body-dark");
         
@@ -105,6 +121,9 @@ function changeTheme() {
                 element.classList.add("btn-dark")
             }
         });
+
+        changeThemeBtn = document.getElementById("changeThemeBtn");
+        changeThemeBtn.innerHTML = sunIcon;
 
         theme = "dark";
     } else {
@@ -132,6 +151,9 @@ function changeTheme() {
                 element.classList.remove("btn-dark")
             }
         });
+
+        changeThemeBtn = document.getElementById("changeThemeBtn");
+        changeThemeBtn.innerHTML = moonIcon;
 
         theme = "light";
     }
